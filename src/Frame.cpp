e27ffe1445432dae4c02234ae5a0dd219f3da165
Nathan Ringo <tikiking1@gmail.com>
2015-06-09 00:38:14 -0500

    Implements Frame.get{Height,Width}

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -9,6 +9,18 @@ void py_Frame_destroy(PyObject *frameCapsule) {
 	delete ((Frame*) PyCapsule_GetPointer(frameCapsule, "Frame"));
 }
 
-// PyObject *py_Frame_getHeight(PyObject *self, PyObject *args);
-// PyObject *py_Frame_getWidth(PyObject *self, PyObject *args);
+PyObject *py_Frame_getHeight(PyObject *self, PyObject *args) {
+	PyObject *frameCapsule = NULL;
+	if(!PyArg_ParseTuple(args, "O", &frameCapsule))
+		return NULL;
+	Frame *frame = (Frame*) PyCapsule_GetPointer(frameCapsule, "Frame");
+	return PyInt_FromSize_t(frame->height);
+}
+PyObject *py_Frame_getWidth(PyObject *self, PyObject *args) {
+	PyObject *frameCapsule = NULL;
+	if(!PyArg_ParseTuple(args, "O", &frameCapsule))
+		return NULL;
+	Frame *frame = (Frame*) PyCapsule_GetPointer(frameCapsule, "Frame");
+	return PyInt_FromSize_t(frame->width);
+}
 // PyObject *py_Frame_getData(PyObject *self, PyObject *args);
